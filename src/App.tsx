fae688aaddd1c7881c965d239d18aefc0b2e956d
Cameron Morris <cameronjmorris87@gmail.com>
2024-07-15 14:54:11 +1200

    calling api on page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Song from "./interfaces/Song";
 import LyricGrid from "./components/LyricGrid";
 import GetLyrics from "./endpoints/GetLyrics";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./styles/App.css";
 
 export default function App() {
@@ -11,17 +11,13 @@ export default function App() {
     lyrics: [],
   });
 
+  useEffect(() => {
+    GetLyrics(setSong);
+  }, []);
+
   return (
     <>
       <div className="lyricGame">
-        <button
-          onClick={async () => {
-            const apiResult = await GetLyrics();
-            setSong(apiResult);
-          }}
-        >
-          Test API
-        </button>
         <div className="gamePanel">
           <LyricGrid song={song} />
         </div>
--- src/endpoints/GetLyrics.tsx
+++ src/endpoints/GetLyrics.tsx
@@ -2,21 +2,20 @@ import axios from "axios";
 import ServerAddr from "../api/server";
 import Song from "../interfaces/Song";
 
-export default async function GetLyrics() {
-  async function getLyric() {
-    try {
-      const url = ServerAddr + "/lyrics";
-      const { data } = await axios.get(url);
-      return data;
-    } catch (error) {
-      throw error;
-    }
+export default function GetLyrics(
+  setSong: React.Dispatch<React.SetStateAction<Song>>
+) {
+  try {
+    const url = ServerAddr + "/lyrics";
+    axios.get(url).then((result) => {
+      const song: Song = {
+        songName: result.data.songName,
+        artist: result.data.artist,
+        lyrics: result.data.lyrics,
+      };
+      setSong(song);
+    });
+  } catch (error) {
+    throw error;
   }
-  const result = await getLyric();
-  const song: Song = {
-    songName: result.songName,
-    artist: result.artist,
-    lyrics: result.lyrics,
-  };
-  return song;
 }
