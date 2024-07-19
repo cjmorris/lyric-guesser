2a625de28f2c4d5060f20a919c7a42f981df92af
Cameron Morris <cameronjmorris87@gmail.com>
2024-07-19 13:16:36 +1200

    made basic header

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Song from "./interfaces/Song";
 import LyricGrid from "./components/LyricGrid/LyricGrid";
 import GetLyrics from "./endpoints/GetLyrics";
 import { useState, useEffect } from "react";
-import "./styles/App.css";
+import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,9 @@
 import "./Header.css";
 
 export default function Header() {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <h1>Lyric Guesser</h1>
+    </div>
+  );
 }
